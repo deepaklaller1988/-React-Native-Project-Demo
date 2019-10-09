7e6393cc87d9fd5f0eb87c064609aa23c6435ef6
Harshdeepk22 <hkaur@contriverz.com>
2019-10-09 19:33:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const speakeasy = require('speakeasy')
-const client = require('twilio')("ACbe0b388ca58323a7ba19e7083b7b2b2e", "cf573d91c507fec56b99fea2e40d7bdf")
+const client = require('twilio')("Account_Id", "Auth_Token")
 const cors = require('cors')
 
 // const accountSid = '';
@@ -19,7 +19,7 @@ app.use(cors())
 app.get('/getOTp/:number',(req,res)=>{
 
 const mobile_number = '+91' + req.params.number;
-client.verify.services('VAa9dab845f972a7744b071dfa5c4b51c7')
+client.verify.services('Service_Id')
              .verifications
              .create({to: mobile_number, channel: 'sms'})
              .then(verification => 
@@ -37,7 +37,7 @@ app.get('/verify/:phone/:otp',(req,res)=>{
 // console.log(phone_Number)
 // console.log(OTP)
 
-     client.verify.services('VAa9dab845f972a7744b071dfa5c4b51c7')
+     client.verify.services('Service_Id')
       .verificationChecks
       .create({to: phone_Number, code: OTP})
       .then(verification_check => {
@@ -74,13 +74,7 @@ app.get('/verify/:phone/:otp',(req,res)=>{
 //     "remaining":(30-Math.floor((new Date().getTime() / 1000.0 % 30)))
 //     })
 
-    // app.post('/createOTp',(req,res)=>{
-    //     var data = client.verify.services('VAa9dab845f972a7744b071dfa5c4b51c7').verifications.create({
-    //         to:req.body.phone,
-    //         channel:'sms'        
-    //     }).then(verification =>{verification.status})
-    // })
-//})
+    /
 
 
 
